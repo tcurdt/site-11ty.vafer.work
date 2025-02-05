9521753429fd786468cbce2508775b39d155a5d5
Torsten Curdt <tcurdt@vafer.org>
2025-02-05 21:48:10 +0100

    tailwind esm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-module.exports = {
+export default {
   content: ["./src/**/*.{html,njk}"],
   theme: {
     extend: {
       fontFamily: {
-        'merriweather': ['Merriweather', 'serif'],
-        'poppins': ['Poppins', 'sans-serif'],
-        'code': ['Source Code Pro', 'monospace'],
+        merriweather: ["Merriweather", "serif"],
+        poppins: ["Poppins", "sans-serif"],
+        code: ["Source Code Pro", "monospace"],
       },
       colors: {
-        'my-gray': '#2a2a2a',
-        'my-orange': '#f3a433'
+        "my-gray": "#2a2a2a",
+        "my-orange": "#f3a433",
       },
     },
   },
