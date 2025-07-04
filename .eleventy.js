b376aaec1116023ed78c7a593106b29b27d5a7cc
Torsten Curdt <tcurdt@vafer.org>
2025-07-04 20:37:37 +0200

    0 is better

--- .eleventy.js
+++ .eleventy.js
@@ -43,7 +43,7 @@ export default function (config) {
     return collection
       .getFilteredByGlob("src/products/*/index.njk")
       .sort(function (a, b) {
-        return (a.data.order || 999) - (b.data.order || 999);
+        return (a.data.order || 0) - (b.data.order || 0);
       });
   });
 
